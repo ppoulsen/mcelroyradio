f575591b0b67a186651b0ca1d00ba266ae8ec579
Paul Poulsen <ppoulsen21@gmail.com>
2018-08-27 20:44:51 -0500

    Use dist/ of RSSParser

--- client/src/rssFeeds/feedResults.js
+++ client/src/rssFeeds/feedResults.js
@@ -1,8 +1,10 @@
-import Parser from 'rss-parser';
+// Need to specify dist since rss-parser currently exposes the source code by default
+/* global RSSParser */
+import 'rss-parser/dist/rss-parser';
 
 import feeds from './feeds';
 
-const parser = new Parser();
+const parser = new RSSParser();
 
 const promises = [];
 feeds.map(feed => parser.parseURL(feed)).forEach(feedPromise => promises.push(feedPromise));
